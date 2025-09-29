f33153b9299641c2a8e304f0e1dd7f34c156d97a
vivoestasbona <teacherwoong@gmail.com>
2025-09-29 18:58:03 +0900

    [quiz][tab2] Add enums, normalize util, and API stubs

---
+++ shared/api/quiz/finishAttempt.js
@@ -0,0 +1,5 @@
+﻿'use server';
+// finishAttempt: 합계/완료 처리 예정
+export async function finishAttempt(/* { attemptId } */) {
+  throw new Error('finishAttempt: not implemented yet');
+}
---
+++ shared/api/quiz/postComment.js
@@ -0,0 +1,5 @@
+﻿'use server';
+// postComment: 결과 화면 한줄평 등록 예정
+export async function postComment(/* { quizId, attemptId, body } */) {
+  throw new Error('postComment: not implemented yet');
+}
